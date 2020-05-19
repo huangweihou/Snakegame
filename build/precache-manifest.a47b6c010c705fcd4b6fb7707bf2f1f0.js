7c615d09e3344989f02824b16e425e3639aa6256
peter <peter@peterde-MacBook-Pro.local>
2020-05-19 16:41:29 +0800

    5/19

---
+++ build/precache-manifest.a47b6c010c705fcd4b6fb7707bf2f1f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2539d759991c6a31586b849be618aef9",
+    "url": "/Snakegame/index.html"
+  },
+  {
+    "revision": "9aa5443f5195d1791a1f",
+    "url": "/Snakegame/static/js/2.a035640d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Snakegame/static/js/2.a035640d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5a29626508225843456",
+    "url": "/Snakegame/static/js/main.1c4726b2.chunk.js"
+  },
+  {
+    "revision": "a25bb80ef5c07deb0d24",
+    "url": "/Snakegame/static/js/runtime-main.d1846aa0.js"
+  }
+]);
